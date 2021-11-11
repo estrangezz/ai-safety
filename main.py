236fca37e8f498280821225a4bc34e809f1b59d7
unknown <w19990605>
2021-11-12 00:16:47 +0800

    my commit

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ transform_test = transforms.Compose([
 ])
 
 trainset = torchvision.datasets.CIFAR10(
-    root='./data', train=True, download=False, transform=transform_train)
+    root='./data', train=True, download=True, transform=transform_train)
 trainloader = torch.utils.data.DataLoader(
     trainset, batch_size=128, shuffle=True, num_workers=0)
 
